5191f47d8e4b24bd39e80b47e26d5cfb3e4bdf58
Matt Haines <mhpahuta@gmail.com>
2022-01-25 14:21:22 -0800

    Add accept and validate player guess, update event handler

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // game variables
-const guessedLetters = document.querySelector('.guessed-letters');
-const guessBtn = document.querySelector('.guess');
+const guessedLettersUL = document.querySelector('.guessed-letters');
+const guessLetterBtn = document.querySelector('.guess');
 const letterInput = document.querySelector('.letter');
 const wordInProgress = document.querySelector('.word-in-progress');
 const remainingGuessesP = document.querySelector('.remaining');
@@ -9,6 +9,7 @@ const message = document.querySelector('.message');
 const playAgainBtn = document.querySelector('.play-again');
 
 let word = 'magnolia';
+const guessedLetters = [];
 
 // display symbols as placeholders for word
 const addPlaceholders = (word) => {
@@ -23,12 +24,45 @@ const addPlaceholders = (word) => {
 
 addPlaceholders(word);
 
+const validatePlayerInput = (input) => {
+  const acceptedLetter = /[a-zA-Z]/;
+
+  if (input.length === 0) { // input empty?
+    message.innerText = 'That there is blank. Enter a letter.';
+  } else if (input.length > 1) { // input more than one letter?
+    message.innerText = 'Just one letter at a time, please.'
+  } else if (!input.match(acceptedLetter)) { // input a non letter character?
+    message.innerText = 'Please enter a letter from A to Z.';
+  } else {
+    return input;
+  }
+};
+
+const makeGuess = (letter) => {
+  letter = letter.toUpperCase();
+  console.log(letter)
+
+  if (guessedLetters.includes(letter)) {
+    message.innerText = `Letter "${letter}" has already been guessed. Try again!`;
+  } else {
+    guessedLetters.push(letter);
+    console.log(guessedLetters);
+  }
+};
+
 
 // Event listeners 
 
-guessBtn.addEventListener('click', (e) => {
+guessLetterBtn.addEventListener('click', (e) => {
   e.preventDefault();
+  message.innerText = ''; // empty message <p>
   const letterGuess = letterInput.value;
-  console.log(letterGuess)
+  const validGuess = validatePlayerInput(letterGuess);
+
+  if (validGuess) {
+    makeGuess(letterGuess) // valid letter, call makeGuess
+  }
+
   letterInput.value = '';
+
 });
